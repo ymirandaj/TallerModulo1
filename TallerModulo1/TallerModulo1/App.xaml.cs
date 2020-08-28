7dcab8ceb816f58cbb95b31c271eb954a45e9f53
Yomar Miranda <yomarmiranda16@gmail.com>
2020-08-27 22:05:05 -0500

    Agregar archivos de proyecto.

---
+++ TallerModulo1/TallerModulo1/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerModulo1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+        //  MainPage = new MainPage();
+
+            MainPage = new Views.Menu();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TallerModulo1/TallerModulo1/Views/Home.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TallerModulo1.Models.Entidades;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TallerModulo1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public List<LocalesAfiliados> Locales;
+        public Home()
+        {
+            InitializeComponent();
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            CargarLocalesAfiliados();   
+        }
+
+        public void CargarLocalesAfiliados()
+        {
+            Locales = new List<LocalesAfiliados>() { 
+                new LocalesAfiliados
+                {
+                    IdLocal="1",
+                    AcercaDe="",
+                    HorarioAtencion="",
+                    ImageUrl="http://18.218.178.167/imagesemail/walmart.jpg",
+                    NombreLocalAfiliado="Walmar"
+                },
+                 new LocalesAfiliados
+                {
+                    IdLocal="2",
+                    AcercaDe="",
+                    HorarioAtencion="",
+                    ImageUrl="http://18.218.178.167/imagesemail/target.jpg",
+                    NombreLocalAfiliado="Target"
+                },
+                 new LocalesAfiliados
+                {
+                    IdLocal="3",
+                    AcercaDe="",
+                    HorarioAtencion="",
+                    ImageUrl="http://18.218.178.167/imagesemail/ferre1.jpg",
+                    NombreLocalAfiliado="Ferreteria"
+                }
+
+
+            };
+
+            CLLocales.ItemsSource = Locales;
+        }
+
+
+
+    }
+}
